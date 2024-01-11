e8760c3e97a6ae7eff883d84d4993aa34581b0c1
Michal Plachta <mic.plachta@gmail.com>
2024-01-11 23:31:03 +0100

    Compiling! Renders a triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,92 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <glad/glad.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include <GL/gl.h>
 
-typedef int32_t i32;
-typedef uint32_t u32;
-typedef int32_t b32;
 
-#define WinWidth 1000
-#define WinHeight 1000
+#define WinWidth 960
+#define WinHeight 640
+
+static const GLfloat g_vertex_buffer_data[] = {
+   -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+   0.0f,  1.0f, 0.0f,
+};
 
 int main (int ArgCount, char **Args)
 {
+  int32_t WindowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+  SDL_Window *Window = SDL_CreateWindow("GP Project",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        WinWidth,
+                                        WinHeight,
+                                        WindowFlags);
+  if(!Window) {
+    std::cout << "Failed to create a window";
+    return -1;
+  }
 
-  u32 WindowFlags = SDL_WINDOW_OPENGL;
-  SDL_Window *Window = SDL_CreateWindow("OpenGL Test", 0, 0, WinWidth, WinHeight, WindowFlags);
-  assert(Window);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
   SDL_GLContext Context = SDL_GL_CreateContext(Window);
-  
-  b32 Running = 1;
-  b32 FullScreen = 0;
+
+  if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
+    std::cout << "Failed to initialize OpenGL context" << std::endl;
+    return -1;
+  }
+
+  int32_t Running = 1;
+  int32_t FullScreen = 0;
+
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
+  GLuint vertexbuffer;
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
   while (Running)
   {
     SDL_Event Event;
+
     while (SDL_PollEvent(&Event))
     {
       if (Event.type == SDL_KEYDOWN)
       {
         switch (Event.key.keysym.sym)
         {
-          case SDLK_ESCAPE:
-            Running = 0;
-            break;
-          case 'f':
-            FullScreen = !FullScreen;
-            if (FullScreen)
-            {
-              SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
-            }
-            else
-            {
-              SDL_SetWindowFullscreen(Window, WindowFlags);
-            }
-            break;
-          default:
-            break;
+          
         }
       }
       else if (Event.type == SDL_QUIT)
       {
         Running = 0;
-      }
+      } 
     }
 
     glViewport(0, 0, WinWidth, WinHeight);
-    glClearColor(1.f, 0.f, 1.f, 0.f);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+      0,
+      3,
+      GL_FLOAT,
+      GL_FALSE,
+      0,
+      (void*)0
+    );
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
 
     SDL_GL_SwapWindow(Window);
   }
