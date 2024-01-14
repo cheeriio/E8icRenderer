5a01a9ae89428856e7f1f7a220dbc3e15fa9b4d9
Michal Plachta <mic.plachta@gmail.com>
2024-01-14 01:40:29 +0100

    Added normal mapping for cube. Looks okayish.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -45,16 +45,16 @@ int main (int ArgCount, char **Args)
   }
 
   glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS); 
+	glDepthFunc(GL_LESS);
+  glEnable(GL_CULL_FACE);
 
   GLuint VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
   GLuint tex_shader;
-  LoadShaders(&tex_shader, "shaders/TexturedVertexShader.vertexshader", NULL, "shaders/TexturedFragmentShader.fragmentshader");
-
-  GLuint matrix_id = glGetUniformLocation(tex_shader, "MVP");
+  // LoadShaders(&tex_shader, "shaders/Light.vertexshader", NULL, "shaders/Light.fragmentshader");
+  LoadShaders(&tex_shader, "shaders/LightNormal.vertexshader", NULL, "shaders/LightNormal.fragmentshader");
 
   // Loading the crate .obj model
   std::vector<glm::vec3> crate_vertices;
@@ -66,27 +66,12 @@ int main (int ArgCount, char **Args)
     return -1;
   }
 
-  std::cout << "Vertices:" << std::endl;
-  for(int i = 0; i < crate_vertices.size(); i++) {
-    std::cout << crate_vertices[i].x << " "
-              << crate_vertices[i].y << " "
-              << crate_vertices[i].z << std::endl;
-  }
+  std::vector<glm::vec3> crate_tangents;
+  std::vector<glm::vec3> crate_bitangents;
 
-  std::cout << "UVs:" << std::endl;
-  for(int i = 0; i < crate_uvs.size(); i++) {
-    std::cout << crate_uvs[i].x << " "
-              << crate_uvs[i].y << std::endl;
-  }
+  ComputeTangents(crate_vertices, crate_uvs, crate_normals, crate_tangents, crate_bitangents);
 
-  std::cout << "Normals:" << std::endl;
-  for(int i = 0; i < crate_normals.size(); i++) {
-    std::cout << crate_normals[i].x << " "
-              << crate_normals[i].y << " "
-              << crate_normals[i].z << std::endl;
-  }
-
-  // Loading the crate .png texture
+  // Loading the crate .png textures
   int crate_w, crate_h, crate_comp;
   unsigned char* crate_image = stbi_load("textures/crate.png", &crate_w, &crate_h, &crate_comp, STBI_rgb_alpha);
 
@@ -95,16 +80,33 @@ int main (int ArgCount, char **Args)
     return -1;
   }
 
+  int crate_n_w, crate_n_h, crate_n_comp;
+  unsigned char* crate_normals_image = stbi_load("textures/crate_normals.png", &crate_n_w, &crate_n_h, &crate_n_comp, STBI_rgb);
+
+  if(crate_normals_image == nullptr) {
+    std::cout << "Failed to load the texture" << std::endl;
+    return -1;
+  }
+
   GLuint Texture;
   glGenTextures(1, &Texture);
   glBindTexture(GL_TEXTURE_2D, Texture);
-  glPixelStorei(GL_UNPACK_ALIGNMENT,1);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, crate_w, crate_h, 0, GL_RGBA, GL_UNSIGNED_BYTE, crate_image);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-  GLuint TextureID  = glGetUniformLocation(tex_shader, "myTextureSampler");
+  GLuint TextureNormals;
+  glGenTextures(1, &TextureNormals);
+  glBindTexture(GL_TEXTURE_2D, TextureNormals);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, crate_n_w, crate_n_h, 0, GL_RGB, GL_UNSIGNED_BYTE, crate_normals_image);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+  GLuint TextureID  = glGetUniformLocation(tex_shader, "DiffuseTextureSampler");
+  GLuint NormalTextureID  = glGetUniformLocation(tex_shader, "NormalTextureSampler");
 
   GLuint vertexbuffer;
   glGenBuffers(1, &vertexbuffer);
@@ -115,6 +117,24 @@ int main (int ArgCount, char **Args)
 	glGenBuffers(1, &uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, crate_uvs.size() * sizeof(glm::vec2), crate_uvs.data(), GL_STATIC_DRAW);
+
+  GLuint normalbuffer;
+  glGenBuffers(1, &normalbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+  glBufferData(GL_ARRAY_BUFFER, crate_normals.size() * sizeof(glm::vec3), crate_normals.data(), GL_STATIC_DRAW);
+
+  GLuint tangentbuffer;
+  glGenBuffers(1, &tangentbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, tangentbuffer);
+  glBufferData(GL_ARRAY_BUFFER, crate_tangents.size() * sizeof(glm::vec3), crate_tangents.data(), GL_STATIC_DRAW);
+
+  GLuint bitangentbuffer;
+  glGenBuffers(1, &bitangentbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, bitangentbuffer);
+  glBufferData(GL_ARRAY_BUFFER, crate_bitangents.size() * sizeof(glm::vec3), crate_bitangents.data(), GL_STATIC_DRAW);
+
+
+  
 
   glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 16.0f / 9.0f, 0.1f, 100.0f);
 	glm::vec3 initialCameraPos(7, 7, -5);
@@ -127,7 +147,15 @@ int main (int ArgCount, char **Args)
 	glm::mat4 Model = glm::mat4(1.0f);
 	glm::mat4 MVP = Projection * View * Model;
 
-  float angle = 0;
+  GLuint matrix_id = glGetUniformLocation(tex_shader, "MVP");
+  GLuint view_matrix_id = glGetUniformLocation(tex_shader, "V");
+	GLuint model_matrix_id = glGetUniformLocation(tex_shader, "M");
+  GLuint model__view_matrix_id = glGetUniformLocation(tex_shader, "MV");
+
+  GLuint light_id = glGetUniformLocation(tex_shader, "LightPosition");
+
+  float angle = 0.0;
+  float angle_light = 0.0;
   int32_t Running = 1;
 
   while (Running)
@@ -146,6 +174,15 @@ int main (int ArgCount, char **Args)
           case SDLK_LEFT:
             angle -= 2.0f;
             break;
+          case SDLK_UP:
+            angle_light += 2.0f;
+            break;
+          case SDLK_DOWN:
+            angle_light -= 2.0f;
+            break;
+          case SDLK_ESCAPE:
+            Running = false;
+            break;
           default:
             break;
         }
@@ -156,6 +193,11 @@ int main (int ArgCount, char **Args)
       } 
     }
 
+    // Rendering the shadow map
+
+
+
+    // Rendering the box
     glm::mat4 rotateX =
 		  glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::vec4 newCameraPos = rotateX * glm::vec4(initialCameraPos, 1);
@@ -165,16 +207,29 @@ int main (int ArgCount, char **Args)
 				glm::vec3(0,1,0)
 			  );
 		MVP = Projection * View * Model;
+    glm::mat3 ModelView = glm::mat3(View * Model);
 
-    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClearColor(0.5f, 0.5f, 0.5f, 0.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glUseProgram(tex_shader);
     glUniformMatrix4fv(matrix_id, 1, GL_FALSE, &MVP[0][0]);
+		glUniformMatrix4fv(model_matrix_id, 1, GL_FALSE, &Model[0][0]);
+		glUniformMatrix4fv(view_matrix_id, 1, GL_FALSE, &View[0][0]);
+    glUniformMatrix3fv(model__view_matrix_id, 1, GL_FALSE, &ModelView[0][0]);
 
-    glActiveTexture(GL_TEXTURE0);
+    glm::mat4 rotateLightX =
+		  glm::rotate(glm::mat4(1.0f), glm::radians(angle_light), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::vec4 lightPos = rotateLightX * glm::vec4(20, 0, 20, 1);
+    glUniform3f(light_id, lightPos.x, lightPos.y, lightPos.z);
+
+		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture);
-    glUniform1i(TextureID, 0);
+		glUniform1i(TextureID, 0);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, TextureNormals);
+		glUniform1i(NormalTextureID, 1);
 
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -198,16 +253,53 @@ int main (int ArgCount, char **Args)
       (void*)0
     );
 
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+    glVertexAttribPointer(
+        2,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        0,
+        (void*)0
+    );
+
+    glEnableVertexAttribArray(3);
+		glBindBuffer(GL_ARRAY_BUFFER, tangentbuffer);
+		glVertexAttribPointer(
+			3,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		glEnableVertexAttribArray(4);
+		glBindBuffer(GL_ARRAY_BUFFER, bitangentbuffer);
+		glVertexAttribPointer(
+			4,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
     glDrawArrays(GL_TRIANGLES, 0, crate_vertices.size() * 3);
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(2);
+    glDisableVertexAttribArray(3);
+    glDisableVertexAttribArray(4);
 
     SDL_GL_SwapWindow(Window);
   }
 
   glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
+  glDeleteBuffers(1, &normalbuffer);
 	glDeleteProgram(tex_shader);
   glDeleteTextures(1, &Texture);
 	glDeleteVertexArrays(1, &VAO);
--- common/include/model.hpp
+++ common/include/model.hpp
@@ -8,4 +8,10 @@ bool LoadOBJ(const char* path,
              std::vector<glm::vec2>& uvs,
              std::vector<glm::vec3>& normals);
 
+void ComputeTangents(std::vector<glm::vec3>& vertices,
+                     std::vector<glm::vec2>& uvs,
+                     std::vector<glm::vec3>& normals,
+                     std::vector<glm::vec3>& tangents,
+                     std::vector<glm::vec3>& bitangents);
+
 #endif // _MODEL_HPP_GP_
